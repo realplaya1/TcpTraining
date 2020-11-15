0d00a47b109c57c01124e5697c5aa6021e55d8a3
realplaya1 <39435205+realplaya1@users.noreply.github.com>
2020-11-15 13:35:04 +0300

    some things

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,10 +10,19 @@ namespace Client
             Connect("127.0.0.1", "String message");
         }
 
+        // Functions:
+        // Connect
+        // SendMessage
+        // startlistenning
+        // +OnMessageReceived
+        // StopListening
+        // Disconnet
+
         static void Connect(String server, String message)
         {
             try
             {
+                #region Connect
                 // Create a TcpClient.
                 // Note, for this client to work you need to have a TcpServer
                 // connected to the same address as specified by the server, port
@@ -21,13 +30,16 @@ namespace Client
                 Int32 port = 13000;
                 TcpClient client = new TcpClient(server, port);
 
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                
 
                 // Get a client stream for reading and writing.
                 //  Stream stream = client.GetStream();
 
                 NetworkStream stream = client.GetStream();
+                #endregion Connect
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
 
                 // Send the message to the connected TcpServer.
                 stream.Write(data, 0, data.Length);
